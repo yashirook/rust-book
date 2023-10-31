d1265fc68ada8eb41071e8c9382ccd90b84b4110
yashirook <yashiro.kentaro@gmail.com>
2023-11-01 00:46:44 +0900

    データ型

---
+++ data_type/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let x = 2.0;
+
+    let y: f32 = 3.0;
+
+    let sum = 5 + 10;
+
+    let difference = 95.5 - 4.3;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3;
+
+    let remainder = 43 % 5;
+
+    let x: (i32, f64, u8) = (500, 6.4, 10000);
+}
