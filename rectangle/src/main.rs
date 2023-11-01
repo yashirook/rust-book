0f3a4423eb52d843dbe227824ee671559e2c4b32
yashirook <yashiro.kentaro@gmail.com>
2023-11-02 07:45:54 +0900

    構造体を使うようリファクタ下

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -4,13 +4,30 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 40 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    println!("rect is {:#?}", rect1);
-    println!("Area: {}", area(rect1));
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height
+            && self.width > other.width
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
-fn area(rectangle: Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 40 };
+    let rect2 = Rectangle { width: 20, height: 30 };
+    let rect3 = Rectangle::square(30);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
